a1f662bfc73580b4355e74cfde03eed08da790f9
birkin <birkin.diana@gmail.com>
2017-10-03 14:37:10 -0400

    stubs view handler.

--- bdpy3_web_app/views.py
+++ bdpy3_web_app/views.py
@@ -2,13 +2,16 @@
 
 import datetime, json, logging, os, pprint
 from bdpy3_web_app import settings_app
+from bdpy3_web_app.lib.app_helper import EzbHelper
 from django.conf import settings
 from django.contrib.auth import logout
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 
+
 log = logging.getLogger(__name__)
+ezb_helper = EzbHelper()
 
 
 def info( request ):
@@ -18,10 +21,21 @@ def info( request ):
 
 def v1( request ):
     """ Handles post from easyborrow & returns json results. """
+    # log.debug( 'request, ```%s```' % pprint.pformat(request.__dict__) )
+    log.debug( '\n\nstarting request...' )
+    if ezb_helper.validate_request( request.method, request.META.get('REMOTE_ADDR', ''), request.META.get('HTTP_AUTHORIZATION', ''), request.GET ) is False:  # for dev; will be POST
+        log.info( 'request invalid, returning 400' )
+        return HttpResponseBadRequest( '400 / Bad Request' )
+    result_data = ezb_helper.do_lookup( flask.request.form )
+    interpreted_response_dct = ezb_helper.interpret_result( result_data )
+    logger.debug( 'returning response' )
+    return flask.jsonify( interpreted_response_dct )
+
     return HttpResponse( 'v1 handling coming' )
 
 
 def access_test( request ):
     """ Returns simplest response. """
+    log.debug( 'request, ```%s```' % pprint.pformat(request.__dict__) )
     now = datetime.datetime.now()
     return HttpResponse( '<p>hi</p> <p>( %s )</p>' % now )
