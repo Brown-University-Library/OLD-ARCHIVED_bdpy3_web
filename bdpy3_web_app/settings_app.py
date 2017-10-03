e3c8359ceb9827372dd201517642c8e0573290d5
birkin <birkin.diana@gmail.com>
2017-10-03 14:36:45 -0400

    adds settings.

--- bdpy3_web_app/settings_app.py
+++ bdpy3_web_app/settings_app.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 
-import os
+import json, os
 
+
+API_AUTHORIZATION_CODE = os.environ['BDPY3WEB__API_AUTHORIZATION_CODE']  # for v1
+API_IDENTITY = os.environ['BDPY3WEB__API_IDENTITY']  # for v1
+
+LEGIT_IPS = json.loads( os.environ['BDPY3WEB__LEGIT_IPS'] )
 
 README_URL = os.environ['BDPY3WEB__README_URL']
+
